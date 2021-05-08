68c628ee03845fbd1091447c49d50402a9f321fd
Awes0meM4n <lonchezas@hotmail.com>
2021-05-08 13:35:22 +0200

    Usar API con https

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   logo: '../assets/img/darthVader.jpg',
   urlDptoSIC: 'https://git.institutomilitar.com/',
-  host: 'http://swapi.dev/api/',
+  host: 'https://swapi.dev/api/',
   itemsPorPagina: 10
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
   production: false,
   logo: '../assets/img/startwars.png',
   urlDptoSIC: 'https://git.institutomilitar.com/',
-  host: 'http://swapi.dev/api/',
+  host: 'https://swapi.dev/api/',
   itemsPorPagina: 10
 };
 
